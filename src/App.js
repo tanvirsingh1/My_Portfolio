a021e266cf84b02129c84983b9bb918e694ca846
tanvirsingh1 <tnvir2182002@gmail.com>
2024-08-13 21:24:49 -0400

    Added Contact

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Layout from "./components/Layouts/layout";
 import About from "./pages/About/about";
 import TechStack from "./pages/TechStack/TechStack";
 import Project from "./pages/Projects/Project";
+import Education from "./pages/Educations/Education";
+import WorkExp from "./pages/workExp/WorkExp";
+import Contact from "./pages/Contact/Contact";
 function App() {
   return (
     < >
       <Layout />
       <div className="container">
         <About />
+        <Education />
         <TechStack />
         <Project />
+        <WorkExp />
+        <Contact />
       </div>
 
     </>
